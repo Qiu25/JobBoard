0517a3997ee72e0b48c5fc429b1b4eab63e022b9
Qiu25 <83657592+Qiu25@users.noreply.github.com>
2024-03-14 14:15:27 +0800

    Update logout.php

--- JobBoard/logout.php
+++ JobBoard/logout.php
@@ -1,9 +1,7 @@
 <?php
   require('conn.php');
 
-  session_start();
-
-  session_destroy();
+  setcookie("user_id", "");
 
   echo "登出成功，即將轉跳";
   die('<meta http-equiv="refresh" content="5; url=index.php">');
